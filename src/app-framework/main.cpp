8c8246bf4c4fbfadbf864b29f3fe9b869c99a8fa
Jay Ravi <jaykumarark@gmail.com>
2018-09-21 19:59:06 -0700

    Make main as entry point for Win32 apps

--- src/app-framework/main.cpp
+++ src/app-framework/main.cpp
@@ -6,11 +6,8 @@ Jay R Ravi
 #include <windows.h>
 #include "application-win32.h"
 
-
-
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int main()
 {
-
     physika::ApplicationWin32 app(_T("Physika App Framework"), 1024, 768); 
     app.Initialize(); 
     app.Run(); 
