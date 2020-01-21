eaf03f5b45d036743c773b1d6cfd428809669403
Jay Ravi <jaykumarark@gmail.com>
2020-01-20 18:57:26 -0800

    Clang formatted code

--- src/simple-project/test/simple-test.cpp
+++ src/simple-project/test/simple-test.cpp
@@ -1,8 +1,8 @@
 #include "gtest/gtest.h"
 
-namespace{
-    TEST(SimpleTest, CheckAddition)
-    {
-        ASSERT_EQ(2, 2);
-    }
+namespace {
+TEST(SimpleTest, CheckAddition)
+{
+    ASSERT_EQ(2, 2);
 }
+}  // namespace
